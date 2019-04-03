d53635e494b1ba7ea466cd44947494813023b2a1
faxedexcel <chinh.le23@gmail.com>
2019-04-03 07:26:05 -0400

    added Kreator, Category, & Quiz models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Kreator, Category, Quiz
 
 # Register your models here.
+@admin.register(Kreator)
+class KreatorAdmin(admin.ModelAdmin):
+   list_display = ('name',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+   list_display = ('name',)
