86432ec1b1d97e425427a96638316de22d3f1da3
Yifaa Yapuncich <yifaasy@gmail.com>
2019-04-06 23:13:10 -0400

    made correc, incorrect buttons that hide

--- core/static/main.js
+++ core/static/main.js
@@ -2,6 +2,10 @@
 const slug = document.URL.split('/')[5]
 const cardDataUrl = `/core/quiz/${slug}/get_card_data/`
 const cardDisplay = query('.card-display')
+const cardFront = query('.card-front')
+const cardBack = query('.card-back')
+let correctButton = query('.ask-if-correct')
+let incorrectButton = query('.ask-if-wrong')
 
 function query (selector) {
   return document.querySelector(selector)
@@ -9,6 +13,12 @@ function query (selector) {
 
 function queryAll (selector) {
   return document.querySelectorAll(selector)
+}
+
+function hideButtons () {
+  correctButton.hidden = true
+  incorrectButton.hidden = true
+  console.log('correcthide')
 }
 
 function getDeckCards (cardDataUrl) {
@@ -27,16 +37,21 @@ function getQuestionAnswer (cardDataUrl) {
       for (let card of Object.values(cardData)) {
         query('.question-button').addEventListener('click', function () {
           let idx = Math.floor(Math.random() * card.length)
-          cardDisplay.innerText = card[idx][0]
+          cardFront.innerText = card[idx][0]
           query('.answer-button').addEventListener('click', function () {
-            cardDisplay.innerText = card[idx][1]
+            cardBack.innerText = card[idx][1]
           })
         })
       }
     })
 }
 
+// function tallyCorrect () {
+//   let score = 0
+// }
+
 document.addEventListener('DOMContentLoaded', function () {
+  hideButtons()
   getDeckCards(cardDataUrl)
   getQuestionAnswer(cardDataUrl)
 })
