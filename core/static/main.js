b3c7633aef598fbb869d3ba8c74b5e0cd4b569f1
bjgribb <bjgribb@gmail.com>
2019-04-07 13:17:44 -0400

    updated quiz so it randomly shuffles cards and removes the card after you cycle thru it.

--- core/static/main.js
+++ core/static/main.js
@@ -2,6 +2,7 @@
 const slug = document.URL.split('/')[5]
 const cardDataUrl = `/core/quiz/${slug}/get_card_data/`
 const cardDisplay = query('.card-display')
+const questionButton = query('.question-button')
 
 function query (selector) {
   return document.querySelector(selector)
@@ -11,6 +12,7 @@ function queryAll (selector) {
   return document.querySelectorAll(selector)
 }
 
+// function that fetchs from URL
 function getDeckCards (cardDataUrl) {
   let promise = fetch(cardDataUrl).then(function (response) {
     if (!response.ok) {
@@ -21,48 +23,49 @@ function getDeckCards (cardDataUrl) {
   return promise
 }
 
-// function getQuestionAnswer (cardDataUrl) {
-//   getDeckCards(cardDataUrl)
-//     .then(cardData => {
-//       for (let card of Object.values(cardData)) {
-//         query('.question-button').addEventListener('click', function () {
-//           let idx = Math.floor(Math.random() * card.length)
-//           cardDisplay.innerText = card[idx][0]
-//           query('.answer-button').addEventListener('click', function () {
-//             cardDisplay.innerText = card[idx][1]
-//           })
-//         })
-//       }
-//     })
-// }
+// courtesy https://gomakethings.com/how-to-shuffle-an-array-with-vanilla-js/ - shuffles deck on refresh
+function shuffle (array) {
+  var currentIndex = array.length
+  var temporaryValue, randomIndex
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+  return array
+}
 
 function getQuestionAnswer (cardDataUrl) {
   getDeckCards(cardDataUrl)
     .then(cardData => {
-      for (let card of Object.values(cardData)) {
-        query('.question-button').addEventListener('click', function () {
-          const idx = setIdx(card)
-          cardDisplay.innerText = card[idx][0]
-          showAnswer(card, idx)
-        })
-      }
+      let array = (Object.values(cardData)[0])
+      let shuffledDeck = (shuffle(array))
+      showQuestion(shuffledDeck)
+      showAnswer(shuffledDeck)
     })
 }
 
-// function showQuestion (card, idx) {
-//   query('.question-button').addEventListener('click', function () {
-//     cardDisplay.innerText = card[idx][0]
-//   })
-// }
-
-function setIdx (card) {
-  let idx = Math.floor(Math.random() * card.length)
-  return idx
+function showAnswer (shuffledDeck) {
+  query('.answer-button').addEventListener('click', function () {
+    cardDisplay.innerText = shuffledDeck[0][1]
+    shuffledDeck.shift()
+  })
 }
 
-function showAnswer (card, idx) {
-  query('.answer-button').addEventListener('click', function () {
-    cardDisplay.innerText = card[idx][1]
+function showQuestion (shuffledDeck) {
+  questionButton.addEventListener('click', function () {
+    if (shuffledDeck.length === 0) {
+      cardDisplay.innerText = "You've finished!"
+      questionButton.innerText = 'Done'
+    } else {
+      cardDisplay.innerText = shuffledDeck[0][0]
+      questionButton.innerText = 'Next Question'
+    }
   })
 }
 
