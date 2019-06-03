0eb4cb46d46849a73b6ba6e6507c71979840d361
bjgribb <bjgribb@gmail.com>
2019-06-03 16:02:33 -0400

    added dj_database_url

--- flashcard/settings.py
+++ flashcard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -149,3 +150,10 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 
 LOGIN_REDIRECT_URL = '/core/'
+
+from decouple import config
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
