d8824ee430bf039fe9cb15fe94e6b14aef2e0afc
bjgribb <bjgribb@gmail.com>
2019-06-15 17:20:02 -0400

    broke out startGame function

--- core/static/main.js
+++ core/static/main.js
@@ -1,18 +1,11 @@
-
-// const drake = require('dragula')
-// const justFlipIt = require('justflipit')
-
 const slug = document.URL.split('/')[5]
 const cardDataUrl = `/home/quiz/${slug}/get_card_data/`
-const cardDisplay = query('.card-display')
-const questionButton = query('.question-button')
 const scoreDisplay = query('.score-display')
 const card = query('.card-main-container')
 const cardFront = query('.card-front')
 const cardBack = query('.card-back')
-const answerButton = query('.answer-button')
+const startButton = query('.start-button')
 let correctButton = query('.ask-if-correct')
-let incorrectButton = query('.ask-if-wrong')
 let numCorrect = 0
 
 function query (selector) {
@@ -61,26 +54,16 @@ function getQuestionAnswer (cardDataUrl) {
       let array = (Object.values(cardData)[0])
       let shuffledDeck = (shuffle(array))
       const totalDeck = shuffledDeck.length
+      startGame(shuffledDeck)
       showQuestion(shuffledDeck)
-      showAnswer(shuffledDeck)
       updateScore(numCorrect, shuffledDeck, totalDeck)
     })
 }
 
-// function showAnswer (shuffledDeck) {
-//   query('.answer-button').addEventListener('click', function () {
-//     cardBack.innerHTML = `<p>${shuffledDeck[0][1]}</p>
-//                           <p class='ask-if-correct'>Correct?</p>
-//                           <i class="fas fa-times-circle"></i>`
-//     shuffledDeck.shift()
-//   })
-// }
-
 function showQuestion (shuffledDeck) {
   document.addEventListener('click', function (event) {
     if (event.target.classList.contains('ask-if-correct') ||
-    (event.target.classList.contains('ask-if-wrong') ||
-    (event.target.classList.contains('question-button')))) {
+    (event.target.classList.contains('ask-if-wrong'))) {
       if (shuffledDeck.length === 0) {
         cardFront.innerText = "You've finished!"
         cardBack.innerText = "You've finished!"
@@ -92,6 +75,17 @@ function showQuestion (shuffledDeck) {
         shuffledDeck.shift()
       }
     }
+  })
+}
+
+function startGame (shuffledDeck) {
+  startButton.addEventListener('click', function () {
+    hideButtons(startButton)
+    cardFront.innerText = shuffledDeck[0][0]
+    cardBack.innerHTML = `<p>${shuffledDeck[0][1]}</p>
+                          <i class="fas fa-check-circle ask-if-correct"></i>
+                          <i class="fas fa-times-circle ask-if-wrong"></i>`
+    shuffledDeck.shift()
   })
 }
 
@@ -108,9 +102,11 @@ function updateScore (numCorrect, shuffledDeck, totalDeck) {
 }
 
 function flipCard () {
-  card.addEventListener('click', function () {
-    card.classList.toggle('flip')
-  })
+  if (card) {
+    card.addEventListener('click', function () {
+      card.classList.toggle('flip')
+    })
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
